81a174af55e68dd481a6c09879925ce2295ddbe7
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-08-28 20:51:42 +0200

    Revert "Merge branch 'dev' into removeOnStart"
    This reverts commit e1007c201e82066794df31fd18a7b6764a7481ff, reversing
    changes made to e4ea407e69b3b8a3e84c5026daace62c61a17f5f.

--- ShortsRemover/popup.js
+++ ShortsRemover/popup.js
@@ -1,4 +1,3 @@
-
   document.getElementById('changeColor').addEventListener('click', () => {
     // Query the active tab in the current window
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -7,15 +6,9 @@
         target: { tabId: tabs[0].id },
         function: () => {
           // This function will run in the context of the webpage
-          alert("Hello, Img!");
+          console.log("All : popup start removing");
           const elements = document.querySelectorAll('.style-scope ytd-rich-section-renderer');
           elements.forEach(element => {
-            element.style.display = 'none';
-            element.style.backgroundColor = 'red';
-          });
-
-          const elements2 = document.querySelectorAll('.style-scope ytd-reel-shelf-renderer');
-          elements2.forEach(element => {
             element.style.display = 'none';
           });
         }
