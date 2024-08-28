6e943ddd72dcc89efcc88a12fc3599f67bf4900e
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2024-08-28 20:40:57 +0200

    add class

--- ShortsRemover/popup.js
+++ ShortsRemover/popup.js
@@ -1,6 +1,4 @@
 
-  
-
   document.getElementById('changeColor').addEventListener('click', () => {
     // Query the active tab in the current window
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -11,6 +9,12 @@
           // This function will run in the context of the webpage
           alert("Hello, Img!");
           const elements = document.querySelectorAll('.style-scope ytd-rich-section-renderer');
+          elements.forEach(element => {
+            element.style.display = 'none';
+            element.style.backgroundColor = 'red';
+          });
+
+          elements = document.querySelectorAll('.style-scope ytd-reel-shelf-renderer');
           elements.forEach(element => {
             element.style.display = 'none';
             element.style.backgroundColor = 'red';
